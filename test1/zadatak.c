a3e92bbad95071c863caf115ed52ce2a9d31e23b
jia <jelenagjjz@gmail.com>
2019-11-02 15:15:13 +0000

    Prvi zadatak za ocenjivanje, verzija 2.0

--- test1/zadatak.c
+++ test1/zadatak.c
@@ -7,7 +7,10 @@ int main(void) {
   int led, switches = 0;
   char *str;
   char *leds;
-  char en, add_or_sub;
+  char en;
+  char add_or_sub = 0;
+  char key_pressed = 0;
+  int btn_value = 0;
   size_t num_of_bytes = 6;
 
   leds = (char *)malloc(3);
@@ -29,18 +32,52 @@ int main(void) {
 
     en = str[5] - 48;
     switches = 4 * (str[2] - 48) + 2 * (str[3] - 48) + (str[4] - 48);
+    free(str);
 
+    //Citanje tastera za promenu operacije
+    fp = fopen("/dev/button", "r");
+    if (fp == NULL) {
+      puts("Problem pri otvaranju fajla /dev/button\n");
+      return -1;
+    }
+
+    str = (char *)malloc(num_of_bytes+1);
+    getline(&str, &num_of_bytes, fp);
+    btn_value = str[5] - 48; //Vrednost BTN0 tastera
+    free(str);
+
+    if (fclose(fp)) {
+      puts("Problem sa zatvaranjem /dev/button\n");
+      return -1;
+    }
+
+    //Softversko diferenciranje (Da li uopste treba?)
+    if (!key_pressed && btn_value) {
+      key_pressed = 1;
+      add_or_sub = (add_or_sub + 1) % 2; //Variranje promenljive add_or_sub svakim pritiskom tastera
+    }
+
+    if (!btn_value)
+      key_pressed = 0; 
+
+    //Sabiranje / oduzimanje u zavisnosti od vrednosti sub_or add i provera za overflow
     if (en) {
-      led += switches;
-      if (led > 15)
-	led = 15;
-    } else {
-      led = 0;
-    } //kraj if-else za en
+      if (!add_or_sub) {
+	led += switches;
+	if (led > 15)
+	  led = 15;
+      } else {
+	if (led < switches) {
+	  led = 0;
+	} else {
+	  led -= switches; //dodato da ne bi char odlazio u negativne brojeve
+	}
+      }
+    } 
 
-    sprintf(leds, "%d\n", led);
-    puts(leds);
-
+    sprintf(leds, "%d\n", led); //Formiranje stringa koji ce se ispisati u fajl za diode
+    
+    //Ispis na diode
     fp = fopen("/dev/led", "w");
     if (fp == NULL) {
       puts("Problem pri otvaranju /dev/led\n");
@@ -48,10 +85,16 @@ int main(void) {
     }
     
     fputs(leds, fp);
-    fclose(fp);
+    if(fclose(fp)) {
+      puts("Problem sa zatvaranjem /dev/led\n");
+      return -1;
+    }
+    
     sleep(2);
     
   }  //kraj while(1)
+
+  free(leds);
 
   return 0;
 } //kraj main
