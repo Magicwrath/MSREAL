dee9ba4c10b0ff429a8eed362e2d687b8f96826d
Magicwrath <ivezicmilan97@gmail.com>
2019-12-04 21:52:35 +0100

    Dodate funckije upisa i ispisa, neispravne

--- test2/Fifo.c
+++ test2/Fifo.c
@@ -20,7 +20,7 @@ static struct device *my_device;
 static struct cdev *my_cdev;
 
 DECLARE_WAIT_QUEUE_HEAD(readQ);
-DECLARE_WAIT_QUEUE_HEAD(waitQ);
+DECLARE_WAIT_QUEUE_HEAD(writeQ);
 struct semaphore sem;
 
 int fifo[10];
@@ -30,6 +30,7 @@ int endRead = 0;
 
 //rpos == wpos => fifo prazan
 //(rpos+1)%10 == wpos => fifo pun
+//ideja za popravku, dodaj promenljivu koja oznacava slobodna mesta!
 
 int fifo_open(struct inode *pinode, struct file *pfile);
 int fifo_close(struct inode *pinode, struct file *pfile);
@@ -50,15 +51,89 @@ int fifo_open(struct inode *pinode, struct file *pfile) {
 }
 
 int fifo_close(struct inode *pinode, struct file *pfile) {
-  printk(KERN_INFO "Succesfully close fifo\n");
+  printk(KERN_INFO "Succesfully closed fifo\n");
   return 0;
 }
 
 ssize_t fifo_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset) {
-  return 0;
+  int ret;
+  char buff[BUFF_SIZE];
+  long int len = 0;
+
+  if (endRead) {
+    endRead = 0;
+    return 0;
+  }
+
+  if(down_interruptible(&sem)) {
+    return -ERESTARTSYS;
+  }
+
+  while (rpos == wpos) {
+    up(&sem);
+    if (wait_event_interruptible(readQ, (rpos != wpos))) {
+      return -ERESTARTSYS;
+    }
+    if (down_interruptible(&sem)) {
+      return -ERESTARTSYS;
+    }
+  }
+
+  //Prodje proces koji dobije semafor, ako bafer nije prazan
+  if (rpos != wpos) {
+    len = scnprintf(buff, BUFF_SIZE, "%d", fifo[rpos]); //cita sa rpos mesta
+    ret = copy_to_user(buffer, buff, len);
+    rpos = (rpos + 1) % 10;
+    if (ret)
+      return -EFAULT;
+    printk(KERN_INFO "Successfully read\n");
+  } else {
+    printk(KERN_INFO "Fifo is empty\n");
+  }
+
+  up(&sem);
+  wake_up_interruptible(&writeQ);
+
+  endRead = 1;
+  return len;
 }
 
 ssize_t fifo_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset) {
+  int ret = 0;
+  int value;
+  char buff[BUFF_SIZE];
+
+  ret = copy_from_user(buff, buffer, length);
+  if (ret)
+    return -EFAULT;
+
+  buff[length-1] = '\0';
+  if (down_interruptible(&sem))
+    return -ERESTARTSYS;
+
+  while (((rpos + 1)%10) == wpos) {
+    up(&sem);
+    if (wait_event_interruptible(writeQ, (((rpos + 1)%10) == wpos)))
+      return -ERESTARTSYS;
+    if (down_interruptible(&sem))
+      return -ERESTARTSYS;
+  }
+
+  if (((rpos + 1)%10) != wpos) {
+    ret = sscanf(buff, "%d", &value);
+    if (ret == 1) {
+      printk(KERN_INFO "Succesfully wrote value %d", value);
+      fifo[wpos] = value;
+      wpos = (wpos + 1) % 10;
+    } else {
+      printk(KERN_WARNING "Wrong command format\n");
+    }
+  } else {
+    printk(KERN_WARNING "Fifo is full\n");
+  }
+
+  up(&sem);
+  wake_up_interruptible(&readQ);
   return length;
 }
 
@@ -103,6 +178,7 @@ static int __init fifo_init(void) {
   }
   printk(KERN_INFO "cdev added\n");
   printk(KERN_INFO "Driver initialized\n");
+  return 0;
 
  fail_2:
   device_destroy(my_class, my_dev_id);
