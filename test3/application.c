ea279c18e88325fca418caa99652bb783154f68c
root <root@localhost.localdomain>
2020-01-26 14:31:26 +0000

    Version with working gameplay, smiley yet to be implemented

--- test3/application.c
+++ test3/application.c
@@ -1,15 +1,30 @@
-#include "draw.h"
 
+#include "draw.h"
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <unistd.h>
 #include <time.h>
 
+#define CLR_WHITE 0xffff
+#define CLR_RED 0xf800
+#define CLR_GREEN 0x0be0
+
+
+void drawSymbol(int sel, int pos_x, int pos_y, unsigned int* display, unsigned int rgb);
+
 int main(void) {
   int *p;
   int fd;
+  char button_val[7]; //values read from the /dev/button driver
+  int button_pressed = 0; //indicates whether a button is pressed or not
+  int pos_x, pos_y; //coordinates used for drawing symbols during generation
+  int game_over = 0; //indicates whether the player lost
+  int game_begin = 0; //0 for the beggining of the game, after the symbols dissapear turns to 1
+  int exit_game = 0; //variable used for indicating if the player wants to exit the game
+  int gen_sequence[6]; //a variable to store the generated symbol sequence
+  int i = 0; //counter for loops
   FILE *fbutton;
-  unsigned int display[680*480];
+  unsigned int display[640*480];
 
   fd = open("/dev/vga_dma", O_RDWR | O_NDELAY);
   if(fd < 0) {
@@ -17,19 +32,169 @@ int main(void) {
     return -1;
   }
 
+  fbutton = fopen("/dev/button", "r");
+  if(fbutton == NULL) {
+    printf("Can't open /dev/button!\n");
+    return -1;
+  }
+
   //pointer to the virtual memory mapped to the driver memory space
-  p = (int*) mmap(0, MAX_PKT_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, 0);
-
+  p = (int*) mmap(0, MAX_PKT_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED,fd, 0);
+  
+  //Clear the screen
   clearScreen(display);
+  
+  //Main application
+  while(!exit_game) {
+    if(!game_begin) {
+      //--------------------BEGGINING OF THE GAME-----------------------------------
+      //set pseudo random number generator seed to current time
+      srand(time(0));
+      pos_x = 1;
+      pos_y = 1;
 
-  drawSquare(display, 1, 1, 30, 0xffff);
-  drawCross(display, 50, 1, 30, 0xffff);
-  drawTriangle(display, 100, 1, 30, 0xffff);
-  drawScreen(display, 150, 51, 50, 0xffff);
+      //symbol generator loop
+      for(i = 0; i < 6; i++) {
+	gen_sequence[i] = rand() % 4;
+	drawSymbol(gen_sequence[i], pos_x, pos_y, display, CLR_WHITE);
 
-  memcpy(p, display, MAX_PKT_SIZE);
-  munmap(p, MAX_PK_SIZE);
+	//offset the next symbol, if the generated symbol is a triangle, use a different offset
+	if(gen_sequence[i] == 2) {
+	  pos_x += 90;
+	} else {
+	  pos_x += 50;
+	}
+      }
+
+      memcpy(p, display, MAX_PKT_SIZE);
+
+      //sleep for two seconds, then clear the screen
+      sleep(2);
+      clearScreen(display);
+      pos_x = 1; //reset the value
+      i = 0; //reset the counter for the game
+      memcpy(p, display, MAX_PKT_SIZE);
+      game_begin = 1;
+    } else {
+      //----------------------PLAYING THE GAME--------------------------------------------
+
+      fread(button_val, 1, 7, fbutton);
+      button_val[6] = '\0';
+      
+      //software differentiation
+      if(button_pressed == 0 && (strncmp("0b0000", button_val, 6) != 0)) {
+	button_pressed = 1;
+
+	//find out which button is pressed
+	if(strncmp("0b0001", button_val, 6) == 0) {
+	  //BTN0 pressed
+	  if(gen_sequence[i] == 3) {
+	    drawSymbol(3, pos_x, pos_y, display, CLR_GREEN);
+	  }
+	  else {
+	    drawSymbol(3, pos_x, pos_y, display, CLR_RED);
+	    game_over = 1;
+	  }
+	  memcpy(p, display, MAX_PKT_SIZE);
+	  //offset next symbol
+	  pos_x += 50;
+	  i++;
+	}
+      
+
+	if(strncmp("0b0010", button_val, 6) == 0) {
+	  //BTN1 pressed
+	  if(gen_sequence[i] == 2) {
+	    drawSymbol(2, pos_x, pos_y, display, CLR_GREEN);
+	  }
+	  else {
+	    drawSymbol(2, pos_x, pos_y, display, CLR_RED);
+	    game_over = 1;
+	  }
+	  memcpy(p, display, MAX_PKT_SIZE);
+	  //offset next symbol
+	  pos_x += 90;
+	  i++;
+	}
+
+	if(strncmp("0b0100", button_val, 6) == 0) {
+	  //BTN2 pressed
+	  if(gen_sequence[i] == 1) {
+	    drawSymbol(1, pos_x, pos_y, display, CLR_GREEN);
+	  }
+	  else {
+	    drawSymbol(1, pos_x, pos_y, display, CLR_RED);
+	    game_over = 1;
+	  }
+	  //offset next symbol
+	  memcpy(p, display, MAX_PKT_SIZE);
+	  pos_x += 50;
+	  i++;
+	}
+
+	if(strncmp("0b1000", button_val, 6) == 0) {
+	  //BTN3 pressed
+	  if(gen_sequence[i] == 0) {
+	    drawSymbol(0, pos_x, pos_y, display, CLR_GREEN);
+	  }
+	  else {
+	    drawSymbol(0, pos_x, pos_y, display, CLR_RED);
+	    game_over = 1;
+	  }
+	  //offset next symbol
+	  memcpy(p, display, MAX_PKT_SIZE);
+	  pos_x += 50;
+	  i++;
+	}
+
+      } //end of the first part of software diff.
+
+      //check if all the buttons have been released
+      if(strncmp("0b0000", button_val, 6) == 0)
+	button_pressed = 0;
+
+      //button combination for exiting the game
+      if(strncmp("0b1111", button_val, 6) == 0)
+	exit_game = 1;
+	
+      
+      //end of user input
+      if(i == 6) {
+	drawCircle(display, 320, 240, 100, CLR_RED | CLR_GREEN);
+	memcpy(p, display, MAX_PKT_SIZE);
+	sleep(5);
+	clearScreen(display);
+	memcpy(p, display, MAX_PKT_SIZE);
+	game_begin = 0;
+      }
+      
+    } //end of else part for playing the game, game_begin condition
+  } //end of while(!exit_game)
+
+  munmap(p, MAX_PKT_SIZE);
+  fclose(fbutton);
   close(fd);
 
   return 0;
+}
+
+
+void drawSymbol(int sel, int pos_x, int pos_y, unsigned int* display, unsigned int rgb) {
+  switch(sel) {
+  case 0:  
+    drawSquare(display, pos_x, pos_y, 40, rgb);
+    break;
+  case 1:
+    drawCross(display, pos_x, pos_y, 40, rgb);
+    break;
+  case 2:
+    drawTriangle(display, pos_x + 40, pos_y, 40, rgb);
+    break;
+  case 3:
+    drawCircle(display, pos_x + 20, 21, 20, rgb);
+    break;
+  default:
+    printf("Wrong select value!\n");
+    break;
+  }
 }
--- test3/draw.c
+++ test3/draw.c
@@ -4,22 +4,24 @@ void drawSquare(unsigned int *display, int pos_x, int pos_y, int length, unsigne
   //Parameter checking
   if((pos_x + length) > 639) {
     printf("Invalid x position or length, the square exceeds the x range of the display!\n");
-    return 0;
+    //return 0;
   }
 
   if((pos_y + length) > 479) {
     printf("Invalid y position or lenght, the square exceeds the y range of the display!\n");
-    return 0;
+    //return 0;
   }
 
   //Draw the horizontal edges of the square
-  for(int i = pos_x; i < (pos_x + length); i++) {
+  int i = pos_x;
+  for(i; i < (pos_x + length); i++) {
     display[640 * pos_y + i] = rgb; //color the dot in the desired color
     display[640 * (pos_y + length - 1) + i] = rgb;
   }
 
   //Draw the vertical edges of the square
-  for(int i = pos_y; y < (pos_y + length); i++) {
+  i = pos_y;
+  for(i; i < (pos_y + length); i++) {
     display[640 * i + pos_x] = rgb;
     display[640 * i + (pos_x + length - 1)] = rgb;
   }
@@ -33,7 +35,8 @@ void drawCross(unsigned int *display, int pos_x, int pos_y, int distance, unsign
   //The cross can be drawn like the y=-x function from the starting point down
 
   //Draw the edges
-  for(int i = 0; i < distance; i++) {
+  int i = 0;
+  for(i; i < distance; i++) {
     display[640 * (pos_y + i) + pos_x + i] = rgb;
     display[640 * (pos_y + i) + pos_x + distance - 1 - i] = rgb;
   }
@@ -43,35 +46,68 @@ void drawTriangle(unsigned int *display, int pos_x, int pos_y, int height, unsig
   //Optional parameter checking, will be added after testing
 
   //Draw the edges
-  for(int i = 0; i < height; i++) {
+  int i = 0;
+  for(i; i < height; i++) {
     //Left edge
-    display[640 * (y_pos + i) + pos_x - i] = rgb;
+    display[640 * (pos_y + i) + pos_x - i] = rgb;
     //Right edge
-    display[640 * (y_pos + i) + pos_x + i] = rgb;
+    display[640 * (pos_y + i) + pos_x + i] = rgb;
     //Bottom edge, left part
-    display[640 * (y_pos + height - 1) + pos_x - i] = rgb;
+    display[640 * (pos_y + height - 1) + pos_x - i] = rgb;
     //Bottom edge, right part
-    display[640 * (y_pos + height - 1) + pos_x + i] = rgb;
+    display[640 * (pos_y + height - 1) + pos_x + i] = rgb;
   }
 }
 
 void drawCircle(unsigned int *display, int pos_x, int pos_y, int r, unsigned int rgb) {
-  float y_val = 0;
-  float common_op = 0;
+  float y_val;
+  int prev_val_pos;
+  int prev_val_neg;
+  float common_op;
+  int quantized_y;
+  int x = pos_x - r;
+  int i;
 
-  //Calculate the y values for the given x values with the analytic formula
-  //y_val = sqrt(r^2 - (x - pos_x) ^ 2) + pos_y
+  for(x; x <= pos_x + r; x++) {
+    common_op = sqrt(r*r - (x - pos_x)*(x - pos_x));
+    y_val = common_op  + pos_y;
+    //printf("Calculated for x = %d, y_val = %f\n", x, y_val);
+    quantized_y = (int) y_val;
+    //printf("Quantized y_val is : %d\n\n", quantized_y);
+    display[640 * quantized_y + x] = rgb;
+    if(x == (pos_x - r))
+      prev_val_pos = quantized_y;
+    //printf("prev_val_pos : %d\n", prev_val_pos);
+    //printf("quantized_y : %d\n", quantized_y);
 
-  for(int x = -r; x <= r; x++) {
-    //common operator in the positive and negative values
-    common_op = sqrt(r^2 - (x - pos_x)^2);
-    //positive values
-    y_val = common_op + pos_y;
-    display[640 * (int)y_val + pos_x + x] = rgb;
+    //draws a verical line from the previous y point to the new point
+    if(prev_val_pos < quantized_y) {
+      for(i = prev_val_pos; i < quantized_y; i++)
+	display[640 * i + x] = rgb;
+    } else {
+      for(i = quantized_y; i < prev_val_pos; i++)
+	display[640 * i + x] = rgb;
+    }
+    prev_val_pos = quantized_y;
 
-    //negative values
+    //the other solutions for the negative value of the square root
     y_val = pos_y - common_op;
-    display[640 * (int)y_val + pos_x + x] = rgb;
+    quantized_y = (int) y_val;
+    if(x == pos_x - r)
+      prev_val_neg = quantized_y;
+    //printf("prev_val_neg : %d\n", prev_val_neg);
+    //printf("quantized_y : %d\n", quantized_y);
+
+    //same function as for the positive solutions
+    if(prev_val_neg < quantized_y) {
+      for(i = prev_val_neg; i < quantized_y;i++)
+	display[640 * i + x] = rgb;
+    } else {
+      for(i = quantized_y; i < prev_val_neg ; i++)
+	display[640 * i + x] = rgb;
+    }
+    prev_val_neg = quantized_y;
+    display[640 * quantized_y + x] = rgb;
   }
 }
 
@@ -79,6 +115,7 @@ void clearScreen(unsigned int *display) {
   int max_x = 639;
   int max_y = 479;
 
-  for(int i = 0; i <= 680 * (max_y) + max_x; i++)
+  int i = 0;
+  for(i; i <= 640 * (max_y) + max_x; i++)
     display[i] = 0;
 }
